ec176b53f15bff627c18a69e97bd3f2cf546dfaf
Vinayak S <vinayaksukhalal@gmail.com>
2024-08-06 22:30:02 +0530

    first commit

--- src/file/file-upload.controller.ts
+++ src/file/file-upload.controller.ts
@@ -3,16 +3,15 @@
 import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { multerOptions } from './multer.config';
-import { Express } from 'express' // Change this line
+import { Multer } from 'multer';
 
 @Controller('files')
 export class FileUploadController {
 
   @Post("upload")
   @UseInterceptors(FileInterceptor('fileName', multerOptions))
-  uploadFile(@UploadedFile() file: Express.Multer.File) { // Change this line
+  uploadFile(@UploadedFile() file: Multer.File) {
     console.log("fileName", file)
-    // You can return the file info or store it and return a reference
     const {filename} = file;
     const extension = filename.split(".")[filename.split(".").length-1]
     return { 
